ec9768907f1fafa41594f39e9491b97811f1233d
Dian Chun Choy <choydianchun@gmail.com>
2025-02-02 00:42:07 +0800

    add basic query and generate sqlc methods

--- db/query.sql
+++ db/query.sql
@@ -1,33 +1,11 @@
-CREATE OR REPLACE FUNCTION update_updated_at() RETURNS TRIGGER AS
-$$
-    BEGIN NEW.updated_at = now();
-    RETURN NEW;
-    END;
-$$ language 'plpgsql';
+-- name: CreateItem :one
+INSERT INTO items (name, clerk_user_id)
+VALUES ($1, $2)
+RETURNING *;
 
-CREATE TABLE items (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    clerk_user_id TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT current_timestamp,
-    updated_at TIMESTAMP DEFAULT NULL,
-    deleted_at TIMESTAMP DEFAULT NULL
-);
-
-CREATE TRIGGER update_updated_at BEFORE UPDATE ON items FOR EACH ROW EXECUTE PROCEDURE update_updated_at();
-
-CREATE INDEX items_clerk_user_id_idx ON items (clerk_user_id);
-
-CREATE TABLE records (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    value TEXT NOT NULL,
-    item_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT current_timestamp,
-    updated_at TIMESTAMP DEFAULT NULL,
-    deleted_at TIMESTAMP DEFAULT NULL
-);
-
-CREATE TRIGGER update_updated_at BEFORE UPDATE ON records FOR EACH ROW EXECUTE PROCEDURE update_updated_at();
-
-CREATE INDEX records_item_id_idx ON records (item_id);
+-- name: ListItemsByUser :many
+SELECT id, name
+FROM items
+WHERE clerk_user_id = $1
+AND deleted_at IS NULL
+ORDER BY created_at DESC;
---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION update_updated_at() RETURNS TRIGGER AS
+$$
+    BEGIN NEW.updated_at = now();
+    RETURN NEW;
+    END;
+$$ language 'plpgsql';
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    clerk_user_id TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TRIGGER update_updated_at BEFORE UPDATE ON items FOR EACH ROW EXECUTE PROCEDURE update_updated_at();
+
+CREATE INDEX items_clerk_user_id_idx ON items (clerk_user_id);
+
+CREATE TABLE records (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    value TEXT NOT NULL,
+    item_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TRIGGER update_updated_at BEFORE UPDATE ON records FOR EACH ROW EXECUTE PROCEDURE update_updated_at();
+
+CREATE INDEX records_item_id_idx ON records (item_id);
